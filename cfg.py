000e01f9bc7cdfd8841b908ef3c77458b887fd63
Daniel Sisco <daniel.sisco@gmail.com>
2010-09-26 22:51:44 -0400

    added version number when creating a default database, re-organized schema2.0

---
+++ cfg.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+#********************************************************************
+# Filename:            cfg.py
+# Authors:             Daniel Sisco
+# Date Created:        7-20-2010
+# 
+# Abstract: Global variables for the Fin-ally project, hopefully just configuration variables
+#
+# Copyright 2008 Daniel Sisco
+# This file is part of Fin-ally.
+#
+# Fin-ally is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Fin-ally is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Fin-ally.  If not, see <http://www.gnu.org/licenses/>.
+#********************************************************************
+
+# activates certain debug messages, especially in dPrint()
+#    0 = no print
+#    1 = print
+DEBUG = 1       
+
+# activates either a pop-up dialogue for new expenses, or uses a static button panel
+#     1 = static button panel 
+#     0 = pop-up dialogue
+NEW_EXPENSE_PANEL = 0
+
+# activates a grid-based context menu in which a user can delete a row
+#    1 = grid-based context menu active
+#    0 = menu inactive
+GRID_CONTEXT_MENU = 0
+
+# FINally version - used for database migration and version checking
+VERSION = (1,1)
---
+++ schema_1_0.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+#********************************************************************
+# Filename:            schema_1_0.py
+# Authors:             Daniel Sisco
+# Date Created:        9/19/2010
+# 
+# Abstract: Version 1.0 of the FINally database schema
+#
+# Version History:  See repository
+#
+# Copyright 2010 Daniel Sisco
+# This file is part of Fin-ally.
+#
+# Fin-ally is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Fin-ally is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Fin-ally.  If not, see <http://www.gnu.org/licenses/>.
+#********************************************************************
+from sqlmigratelite.migrate import MigrateObject
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship, backref
+
+Base = declarative_base()
+version = "1.0"
+dbVer = (1,0)
+
+#********************************************************************
+# Create SQLAlchemy tables in the form of python classes.
+#********************************************************************
+class User(Base):
+    __tablename__ = 'users'
+    id        = Column(Integer, primary_key=True)
+    name      = Column(String)
+    shortName = Column(String)
+    
+    def __repr__(self):
+        return "<User('%s', '%s')>" % (self.name, self.shortName)
+
+#********************************************************************
+class ExpenseType(Base):
+    __tablename__ = 'expenseTypes'
+    
+    id          = Column(Integer, primary_key=True)
+    description = Column(String)
+    
+    def __repr__(self):
+        return "<ExpenseType('%s')>" % (self.description)
+
+#********************************************************************    
+class Expense(Base):
+    __tablename__ = 'expenses'
+    id      = Column(Integer, primary_key=True)
+    amount  = Column(Integer)
+    date    = Column(String)
+    description = Column(String)
+    
+    # define user_id to support database level link and user for class level link
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user    = relationship(User, backref=backref('expenses', order_by=id))
+    
+    expenseType_id = Column(Integer, ForeignKey('expenseTypes.id'))
+    expenseType    = relationship(ExpenseType, backref=backref('expenseTypes', order_by=id))
+    
+    def __repr__(self):
+        return "<Expense('%s', '%s', '%s')>" % (self.amount, self.date, self.description)
+
+#********************************************************************    
+class Version(Base):
+    __tablename__ = 'versions'
+    id = Column(Integer, primary_key=True)
+    minor = Column(Integer)
+    major = Column(Integer)
+    
+    def __repr__(self):
+        return "<Version('%s', '%s')>" % (self.minor, self.major)
+    
+#************************************
+class SchemaObject(MigrateObject):
+    """
+    version 1.0 schema object - defines custom dumpContent and loadContent methods
+    """
+    def __init__(self, dbPath):
+        MigrateObject.__init__(self, dbPath, version)
+        
+    def dumpContent(self):
+        """dumps User and Version tables as-is"""
+        self.localDict['User'] = self.session.query(User).all()
+        self.localDict['ExpenseType'] = self.session.query(ExpenseType).all()
+        self.localDict['Expense'] = self.session.query(Expense).all()
+        
+    def loadContent(self):
+        """no support for v1.0 load at this time"""
+        pass
