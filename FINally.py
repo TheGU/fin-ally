09af9ac6482ac0185121771c26a10d5e0550fc83
Daniel Sisco <daniel.sisco@gmail.com>
2010-10-03 23:17:25 -0400

    Completing menuBar stitch in - removing unused functionality, binding events to the appropriate handlers, adding new icons, removing unused icons.

--- FINally.py
+++ FINally.py
@@ -36,7 +36,7 @@ from wx._core import WXK_F1, WXK_F2
 from editPage import EditPage
 from grid import GraphicsGrid
 from statusBar import CustomStatusBar
-from menuBar import CreateMenu, MENU_STYLE_XP, MENU_STYLE_2007, MENU_STYLE_MY
+from menuBar import CreateMenu
 
 try:
 	from agw import flatmenu as FM
@@ -216,70 +216,9 @@ class GraphicsPage(wx.Panel):
 		self.userList		= self.database.GetSimpleUserList()
 		self.typeList		= self.database.GetExpenseTypeList()
 		
-		# create a panel for the buttons
-		self.buttonPanel  = wx.Panel(self)
-		
-		# configure either a static button panel or a dialogue based on configuration settings
-		if cfg.NEW_EXPENSE_PANEL == 1:		
-			self.entryButton = wx.Button(self.buttonPanel,
-										id = -1,
-										label = "Enter!",
-										pos = (0,0))
-			self.Bind(wx.EVT_BUTTON, self.OnEnterClick, self.entryButton)
-			
-			# create and bind a user selection box
-			self.userSelect   = wx.ComboBox(self.buttonPanel, 
-										    id=-1,
-										    value=self.userList[0],
-										    choices=self.userList,
-							  				pos=(100,0), 
-							  				style=wx.CB_DROPDOWN)
-			self.Bind(wx.EVT_COMBOBOX, self.OnUserSelect, self.userSelect)
-			
-			# create and bind a type selection box
-			self.typeSelect	  = wx.ComboBox(self.buttonPanel, 
-										    id=-1,
-										    value=self.typeList[0],
-										    choices=self.typeList,
-							  				pos=(200,0), 
-							  				style=wx.CB_DROPDOWN)
-			self.Bind(wx.EVT_COMBOBOX, self.OnTypeSelect, self.typeSelect)
-			
-			# create and bind a calendar box
-			self.cal 		  = callib.CalendarCtrl(self.buttonPanel, 
-													-1, 
-													wx.DateTime_Now(), 
-													pos = (600,0),
-							    					style = callib.CAL_SHOW_HOLIDAYS | callib.CAL_SUNDAY_FIRST)
-			self.Bind(callib.EVT_CALENDAR_SEL_CHANGED, self.OnCalSelChanged, self.cal)
-	
-			# create and bind a value entry box
-			self.valueEntry   = wx.TextCtrl(self.buttonPanel, 
-										    -1, 
-										    "0.00", 
-										    pos = (300,0), 
-										    size = (90, 21))
-			self.Bind(wx.EVT_TEXT, self.OnValueEntry, self.valueEntry)
-	
-			# create and bind a description box
-			self.descEntry    = wx.TextCtrl(self.buttonPanel, 
-										    -1, 
-										    "item description", 
-										    pos = (400,0), 
-										    size = (173,21))
-			self.Bind(wx.EVT_TEXT, self.OnDescEntry, self.descEntry)
-		else:
-			# create and bind a new expense button
-			self.newExpenseButton = wx.Button(self.buttonPanel,
-										      id = -1,
-										      label = "New Expense",
-											  pos = (0,0))
-			self.Bind(wx.EVT_BUTTON, self.ShowNewExpenseDialogue, self.newExpenseButton)	
-		
 		# create a sizer for this Panel and add the buttons and the table
 		self.sizer = wx.BoxSizer(wx.VERTICAL)      # define new box sizer	
 		self.sizer.Add(self.grid, 1, wx.GROW)     # add grid (resize vert and horz)
-		self.sizer.Add(self.buttonPanel, 0, wx.ALIGN_LEFT)    # add panel (no resize vert and aligned left horz)
 		self.SetSizer(self.sizer)
 	
 	def ShowNewExpenseDialogue(self, event):
@@ -372,9 +311,6 @@ class AppMainFrame(wx.Frame):
 		self.icon = wx.Icon("img/FINally.ico", wx.BITMAP_TYPE_ICO)
 		self.SetIcon(self.icon)
 		
-		# populate and connect the menuBar
-		CreateMenu(self)
-		
 		self.sb = CustomStatusBar(self)
 		self.SetStatusBar(self.sb)
 
@@ -385,6 +321,9 @@ class AppMainFrame(wx.Frame):
 		self.notebook.AddPage(self.gPage, "Graphics")
 		self.ePage = EditPage(self.notebook)
 		self.notebook.AddPage(self.ePage, "Types + Users")
+
+		# populate and connect the menuBar
+		CreateMenu(self)
 
 		# arrange notebook windows in a simple box sizer
 		self.sizer = wx.BoxSizer()
@@ -404,75 +343,28 @@ class AppMainFrame(wx.Frame):
 	def SetBackgroundColor(self, colorString):
 		self.SetBackgroundColour(colorString)
 		
-	def OnSize(self, event):
-		print "MenuBar OnSize"
-		self._mgr.Update()
-		self.Layout()
-		
 	def OnQuit(self, event):
-		print "MenuBar OnQuit"
 		self._mgr.UnInit()
-		self.Destroy()
+		self.Destroy()      
 
-	def OnStyle(self, event):
-		
-		eventId = event.GetId()
-		
-		if eventId == MENU_STYLE_2007:
-			ArtManager.Get().SetMenuTheme(FM.Style2007)
-		elif eventId == MENU_STYLE_XP:
-			ArtManager.Get().SetMenuTheme(FM.StyleXP)
-		elif eventId == MENU_STYLE_MY:
-			ArtManager.Get().SetMenuTheme(self.newMyTheme)
-		
-		self.menuBar.Refresh()
-		self.Update()        
+	def OnAbout(self, event):
+		msg = "Welcome to FINally!\n\n" + \
+		      "Author: Daniel LaBeau Sisco @ 10/10/2010\n\n" + \
+		      "Please report any bug/requests or improvements\n" + \
+		      "to Daniel Sisco at the following email address:\n\n" + \
+		      "daniel.sisco@gmail.com\n\n" + \
+		      "Please visit FINally at http://code.google.com/p/fin-ally/ for more information\n"
 
-	def OnTransparency(self, event):
-		
-		transparency = ArtManager.Get().GetTransparency()
-		dlg = wx.TextEntryDialog(self, 'Please enter a value for menu transparency',
-		                         'FlatMenu Transparency', str(transparency))
-		
-		if dlg.ShowModal() != wx.ID_OK:
-			dlg.Destroy()
-			return
-		
-		value = dlg.GetValue()
+		dlg = wx.MessageDialog(self, msg, "About FINally",
+		                       wx.OK | wx.ICON_INFORMATION)
+		dlg.ShowModal()
 		dlg.Destroy()
 		
-		try:
-			value = int(value)
-		except:
-			dlg = wx.MessageDialog(self, "Invalid transparency value!", "Error",
-			                       wx.OK | wx.ICON_ERROR)
-			dlg.ShowModal()
-			dlg.Destroy()
-		
-		if value < 0 or value > 255:
-			dlg = wx.MessageDialog(self, "Invalid transparency value!", "Error",
-			                       wx.OK | wx.ICON_ERROR)
-			dlg.ShowModal()
-			dlg.Destroy()
-
-		ArtManager.Get().SetTransparency(value)
-
-	def OnFlatMenuCmd(self, event):
-	
-		print "Received Flat menu command event ID: %d\n"%(event.GetId())
-		event.Skip()
-	
-	def OnAbout(self, event):
-		
-		msg = "This is the About Dialog of the FlatMenu demo.\n\n" + \
-		      "Author: Andrea Gavana @ 03 Nov 2006\n\n" + \
-		      "Please report any bug/requests or improvements\n" + \
-		      "to Andrea Gavana at the following email addresses:\n\n" + \
-		      "andrea.gavana@gmail.com\ngavana@kpo.kz\n\n" + \
-		      "Welcome to wxPython " + wx.VERSION_STRING + "!!"
-
-		dlg = wx.MessageDialog(self, msg, "FlatMenu wxPython Demo",
-		                       wx.OK | wx.ICON_INFORMATION)
+	def OnUnsupported(self, event):
+		msg = "...this feature is unsupported!\n You should complain to Daniel Sisco at the following email address:\n\n" + \
+			  "daniel.sisco@gmail.com\n\n"
+		dlg = wx.MessageDialog(self, msg, "Sorry...",
+							wx.OK)
 		dlg.ShowModal()
 		dlg.Destroy()
 
