b3c63630b11797ae3be6fa09ef5ce1bd20ecbab7
Daniel Sisco <daniel.sisco@gmail.com>
2010-12-21 00:14:17 -0500

    updating threads.py to include a header.

--- threads.py
+++ threads.py
@@ -1,3 +1,29 @@
+#!/usr/bin/env python
+
+#********************************************************************
+# Filename:            threads.py
+# Authors:             Daniel Sisco
+# Date Created:        12-22-2010
+# 
+# Abstract: provides common 'thread' classes for various FINally objects.
+#
+# Copyright 2010 Daniel Sisco
+# This file is part of Fin-ally.
+#
+# Fin-ally is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Fin-ally is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Fin-ally.  If not, see <http://www.gnu.org/licenses/>.
+#********************************************************************
+
 class ExpenseTypeThread():
     """This class supports global ExpenseType updates. It will execute every function
     present in the class variable refreshFuncList when the method RunRefreshFuncs is
